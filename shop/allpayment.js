59666e286d9f1d7d3b0d58ae0b09434987e8e6cc
alga4023 <75517320+alga4023@users.noreply.github.com>
2021-10-21 10:25:09 +0700

    Add files via upload

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ALGANZ BOT
+
+*さ BANK*
+1. BRI 
+	ㅁ 7390-0101-3984-532
+		• AB*** ****F
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0882 9220 1767
+		• AL**** ****E
+2. OVO
+	ㅁ 0882 9220 1767
+		• AL**** ****E
+3. DANA
+	ㅁ 0882 9220 1767
+		• AL**** ****E
+
+*さ PULSA*
+1. SMARTFREN
+	ㅁ 0882 9220 1767
+2. AXIS
+	ㅁ 0831 1534 8443
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
